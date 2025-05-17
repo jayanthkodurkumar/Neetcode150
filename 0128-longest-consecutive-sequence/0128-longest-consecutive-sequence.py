e2659774febfce1d2afd01b941c21aa49c9518c3
Jayanth Kodur Kumar <83784924+jayanthkodurkumar@users.noreply.github.com>
2025-05-17 11:13:49 -0700

    Time: 55 ms (45.15%), Space: 34.4 MB (19.75%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,25 +1,31 @@
+#  NOTES:
+
+# 1. The original sliding window give O(NlogN).
+# 2. So what we can do is we shall use a set.
+# 3. The rule is that in a LCS here the difference between each element is 1 and if two numbers are same, we count it 
+# as 1 only [0,0,1] = 2 elements.
+# 4. Thus we can use a set since only unique values are needed for the count as above.
+# 5. We loop through the set. For an set element to be the starting point of a sequence, it should not have any prev
+# element in the set (i.e) element - 1 should not be there in set.
+# 6. So when we encounter an element which is starting point, we check for lcs condition and update current sequence count.
+# 7. If the element is not a starting point, just skip the iteration.
+
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        # left = 0
-        if len(nums) == 0:
-            return 0
-        nums.sort()
-        currentLength = 1
-        maxLength = 1
-        # print(nums)
-        for right in range(1,len(nums)):
-            diff = nums[right] - nums[right - 1]
-            print(diff)
-            if diff == 0:
-                continue
-            elif diff == 1:
-                currentLength+=1
-                maxLength = max(currentLength,maxLength)
-            else:
-                maxLength = max(currentLength,maxLength)
-                currentLength = 1
+        n = len(nums)
+        lcs = 0
+        if n == 0:
+            return lcs
 
+        elements_set = set(nums)
 
-
-        return maxLength
-        
+        for element in elements_set:
+            current_prev = element - 1
+            if current_prev not in elements_set:
+                current_longest = 1
+                current_next = element + 1
+                while current_next in elements_set:
+                    current_longest += 1
+                    current_next += 1
+                lcs = max(current_longest, lcs)
+        return lcs
