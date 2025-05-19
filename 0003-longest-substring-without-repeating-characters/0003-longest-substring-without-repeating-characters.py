2e23312b52a015908d16b7de97d2210bbf4ee153
Jayanth Kodur Kumar <83784924+jayanthkodurkumar@users.noreply.github.com>
2025-05-19 09:05:59 -0700

    Time: 17 ms (64.15%), Space: 17.9 MB (52.91%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,3 +1,13 @@
+# so the core idea here is :
+
+# "abcabcbb"
+
+# at idx 3 -> ch = 'a'
+# but a is already a part of our window. our goal is to remove all the characters until including
+# the duplicating 'a' before ch. instead of using a freq map, if i know the last idx where i saw this 'a' ,
+# i can just move my left ptr to that idx + 1. The intuititon here is based on the rule that in a 
+# substring window all the chars occur maximum once.
+
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         n = len(s)
