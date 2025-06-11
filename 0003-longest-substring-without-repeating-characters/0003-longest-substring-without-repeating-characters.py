0640b7cbfd2bf443ab1178dc9cac972c04bb4c77
Jayanth Kodur Kumar <83784924+jayanthkodurkumar@users.noreply.github.com>
2025-06-10 18:50:04 -0700

    Time: 18 ms (60.26%), Space: 17.9 MB (52.92%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,28 +1,22 @@
-# so the core idea here is :
-
-# "abcabcbb"
-
-# at idx 3 -> ch = 'a'
-# but a is already a part of our window. our goal is to remove all the characters until including
-# the duplicating 'a' before ch. instead of using a freq map, if i know the last idx where i saw this 'a' ,
-# i can just move my left ptr to that idx + 1. The intuititon here is based on the rule that in a 
-# substring window all the chars occur maximum once.
-
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        left = 0
         n = len(s)
-        if n == 0 or n == 1:
+        if n <=1:
             return n
-        left, right = 0, 0
-        lastSeenIdx = {}
-        maxLen = 0
-        for right in range(n):
-            ch = s[right]
-            if ch in lastSeenIdx and lastSeenIdx[ch] >= left:
-                left = lastSeenIdx[ch] + 1
+        window_set = set()
+        max_window = 0
+        curr_window = 0
 
-            lastSeenIdx[ch] = right
-            currentLen = right - left + 1
-            maxLen = max(currentLen, maxLen)
+        for right in range(0, n):
+            curr_ch = s[right]
+            # print(curr_ch, right)
+            while curr_ch in window_set:
+                window_set.remove(s[left])
+                left += 1
 
-        return maxLen
+            window_set.add(curr_ch)
+            curr_window = right - left + 1
+            max_window = max(curr_window, max_window)
+
+        return max_window
