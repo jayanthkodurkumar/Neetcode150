32595d87f261ae2ed516b8a64732c89ce9db93a0
Jayanth Kodur Kumar <83784924+jayanthkodurkumar@users.noreply.github.com>
2025-06-11 00:23:23 -0700

    Time: 124 ms (74.03%), Space: 78.6 MB (27.41%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -21,9 +21,8 @@ class LRUCache:
         if key in self.cache:
             node = self.cache[key]
             # remove it
-            self.remove(node)
-            # insert it at end
-            self.insert(node)
+            self.move_to_tail(node)
+
             # return node val
             return node.value
 
@@ -32,31 +31,42 @@ class LRUCache:
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            # remove it
-            self.remove(self.cache[key])
+            # move node to tail and update value
+            node = self.cache[key]
+            self.move_to_tail(node)
+            node.value = value
+        else:
+            if len(self.cache) == self.capacity:
+                # move lru at head to tail and then change its <key, value>
+                node = self.head.next
+                self.cache.pop(node.key)
 
-        if len(self.cache) == self.capacity:
-            # remove lru
-            self.remove(self.tail.prev)
+                self.move_to_tail(node)
 
-        # insert node at the start as head's next and update
-        new_node = Node(key, value)
-        self.insert(new_node)
+                node.key = key
+                node.value = value
+                self.cache[key] = node
+            else:
+                node = Node(key, value)
+                self.insert(node)
+                self.cache[key] = node
+
+    def move_to_tail(self, node):
+        # delink the node
+        self.remove(node)
+        self.insert(node)
 
     def remove(self, node):
         node.prev.next = node.next
         node.next.prev = node.prev
 
-        self.cache.pop(node.key)
-
     def insert(self, node):
-        self.cache[node.key] = node
-
-        node.next = self.head.next
-        self.head.next.prev = node
-
-        self.head.next = node
-        node.prev = self.head
+        # connect the node and tail's prev
+        node.prev = self.tail.prev
+        node.prev.next = node
+        # connect node and tail
+        self.tail.prev = node
+        node.next = self.tail
 
 
 # Your LRUCache object will be instantiated and called as such:
